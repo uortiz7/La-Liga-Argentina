b582b5ece08c337979fda0769e19e73d811004e9
uortiz7 <uortiz@frsn.utn.ede.ar>
2025-01-23 16:45:57 -0300

    Cambios con el dvh, usando svh y lvh

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Selecciona todos los elementos con la clase 'container'
-    let containers = document.getElementsByClassName('container');
-
-    // Itera sobre cada elemento y aplica el estilo deseado
-    for (let i = 0; i < containers.length; i++) {
-        containers[i].style.height = '100vh';
-    }
-});
